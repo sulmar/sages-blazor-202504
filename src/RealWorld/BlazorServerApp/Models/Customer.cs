f0818a2224233d7d28adb62790658b272726c368
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-04-02 15:54:21 +0200

    Dodanie pól Description i IsActive do klienta

--- src/RealWorld/BlazorServerApp/Models/Customer.cs
+++ src/RealWorld/BlazorServerApp/Models/Customer.cs
@@ -4,4 +4,6 @@ public class Customer : BaseEntity
 {
     public string Name { get; set; }
     public string City { get; set; }
+    public string Description { get; set; }
+    public bool IsActive { get; set; }
 }
