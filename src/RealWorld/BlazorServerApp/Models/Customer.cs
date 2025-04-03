3d6308f2b249509b008296bc1c05a1b6f9062aff
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-04-03 09:10:19 +0200

    Dodanie walidacji za pomocą DataAnnotation

--- src/RealWorld/BlazorServerApp/Models/Customer.cs
+++ src/RealWorld/BlazorServerApp/Models/Customer.cs
@@ -1,9 +1,25 @@
-﻿namespace BlazorServerApp.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorServerApp.Models;
 
 public class Customer : BaseEntity
 {
+    [Required, StringLength(20, MinimumLength = 3)]
     public string Name { get; set; }
+
+    [Required]
     public string City { get; set; }
+
+    [RegularExpression("^[A-Z]ello$")]
     public string Description { get; set; }
     public bool IsActive { get; set; }
+
+    [Range(0, 1000)]    
+    public decimal Balance { get; set; }
+
+    [MinLength(5)]
+    public string Password { get; set; }
+
+    [Compare(nameof(Password))]
+    public string ConfirmPassword { get; set; }
 }
